1111fe2f61422e2e83748616b4f18f9e380918f6
Claudio Brasser <cbrasser@protonmail.ch>
2026-01-19 09:47:47 +0100

    fixed issue wit amount of failures

--- src/config/criteria.ts
+++ src/config/criteria.ts
@@ -2,20 +2,20 @@ import type { PromotionCriteria } from '../types';
 
 // Promotion criteria for year 1 (as specified)
 export const year1Criteria: PromotionCriteria = {
-  maxFailures: 4, // no more than 4 final grades below 4
+  maxFailures: 3, // no more than 3 final grades below 4
   minAverage: 4, // average above or equal to 4
   maxDeficit: 2, // total sum of grades below 4 is not larger than 2
 };
 
 // Default criteria for other years (can be customized)
 export const year2Criteria: PromotionCriteria = {
-  maxFailures: 4,
+  maxFailures: 3,
   minAverage: 4,
   maxDeficit: 2,
 };
 
 export const year3Criteria: PromotionCriteria = {
-  maxFailures: 4,
+  maxFailures: 3,
   minAverage: 4,
   maxDeficit: 2,
 };
